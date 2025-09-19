dce1ad861e954eae6c46c33019f1d0944ae2ebd1
feelipefreitas <feelipe.freitas97@gmail.com>
2025-09-19 19:56:03 -0300

    Adicionando lógica de filtro

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -2,15 +2,21 @@ import { Component, inject } from '@angular/core';
 import { IProductResponse } from '../../interfaces/product-response';
 import { UserService } from '../../services/user';
 import { take } from 'rxjs';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products {
   products: IProductResponse[] = [];
+  productsListFiltered: IProductResponse[] = [];
+  filterForm = new FormGroup({
+    title: new FormControl(''),
+    status: new FormControl(''),
+  });
 
   private readonly _userService = inject(UserService);
 
@@ -18,7 +24,28 @@ export class Products {
     this._userService.getProducts().pipe(take(1)).subscribe({
       next: (response) => {
         this.products = response.data;
+        this.productsListFiltered = response.data;
       }
     });
+  }
+
+  filterProductsList() {
+    const filterTitle = this.filterForm.value.title?.toLowerCase();
+    const filterStatus = this.filterForm.value.status?.toLowerCase();
+
+    this.productsListFiltered = this.products.filter(
+      (product) => 
+        (!filterTitle || product.title.toLowerCase().includes(filterTitle)) && 
+        (!filterStatus || product.status.toLowerCase().includes(filterStatus))
+    );
+
+    console.log('Lista filtrada', this.productsListFiltered);
+  }
+
+  clearFilter() {
+    this.productsListFiltered = this.products;
+    
+    this.filterForm.reset();
+    this.filterForm.get('status')?.setValue('');
   }
 }
