d0a80895eec441d5728cbfe7ce9c6efad8bcd844
feelipefreitas <feelipe.freitas97@gmail.com>
2025-09-19 10:34:09 -0300

    Criação parcial da lógica de adicionar produto

--- frontend-gestao-marketplace/src/app/pages/new-product/new-product.ts
+++ frontend-gestao-marketplace/src/app/pages/new-product/new-product.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-product',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-product.html',
   styleUrl: './new-product.css'
 })
 export class NewProduct {
+  imageBase64 = '';
+  productForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+  });
 
+  saveProduct() {
+    console.log('Formulário:', this.productForm.value);
+  }
+
+  onFileSelected(event: Event) {
+    console.log(event)
+  }
 }
