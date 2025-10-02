da86f085cdcb1d5283ee5d3cbde2b4f51f6de03b
cristiellesreis <cristielle.santos94@gmail.com>
2025-10-02 00:07:52 -0300

    feat:  Listagem e renderização dinâmica de produtos

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { Header } from "../../components/header/header";
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products';
+import { IProductResponse } from '../../interfaces/product-response';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-products',
-  imports: [Header],
+  imports: [],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
-export class Products {
+export class Products implements OnInit {
+  products: IProductResponse[] = [];
+  private readonly _productsService = inject(ProductsService);
 
+  ngOnInit() {
+    this._productsService.getProducts().pipe(take(1)).subscribe({
+      next: (response) => {
+
+        this.products = response.data;
+      },
+    })
+  }
 }
