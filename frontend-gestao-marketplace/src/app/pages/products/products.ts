c14c2a00c55529fc9731cfe355098b1f4a51e4e7
cristiellesreis <cristielle.santos94@gmail.com>
2025-10-02 14:50:40 -0300

    feat: filtra e lista produtos

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -2,15 +2,23 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products';
 import { IProductResponse } from '../../interfaces/product-response';
 import { take } from 'rxjs';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products implements OnInit {
+
+
   products: IProductResponse[] = [];
+  filteredProducts: IProductResponse[] = [];
+  filterForm = new FormGroup({
+    title:new FormControl(''),
+    status: new FormControl(''),
+  })
   private readonly _productsService = inject(ProductsService);
 
   ngOnInit() {
@@ -18,7 +26,25 @@ export class Products implements OnInit {
       next: (response) => {
 
         this.products = response.data;
+        this.filteredProducts = response.data;
       },
     })
+  }
+
+  filterProducts() {
+    const title = this.filterForm.value.title?.toLocaleLowerCase();
+    const status = this.filterForm.value.status?.toLocaleLowerCase();
+
+    this.filteredProducts = this.products.filter((product) =>
+      (!title || product.title.toLowerCase().includes(title)) &&
+      (!status || product.status.toLowerCase().includes(status))
+    );
+  }
+
+  clearFilter() {
+    this.filterForm.reset();
+    this.filterForm.get('status')?.setValue('');
+
+    this.filteredProducts = this.products;
   }
 }
