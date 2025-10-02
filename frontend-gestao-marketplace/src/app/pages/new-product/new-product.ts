03606efae1ee43c73ac55bc949f3c899b69380e7
cristiellesreis <cristielle.santos94@gmail.com>
2025-10-02 16:57:10 -0300

    feat: implementa rota para o btn cancelar produto

--- frontend-gestao-marketplace/src/app/pages/new-product/new-product.ts
+++ frontend-gestao-marketplace/src/app/pages/new-product/new-product.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { ProductsService } from '../../services/products';
 import { INewProductRequest } from '../../interfaces/new-products-request';
 import { take } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-product',
@@ -21,6 +22,7 @@ export class NewProduct {
   });
 
   private readonly _productsService = inject(ProductsService);
+  private readonly _router = inject(Router);
 
   saveProduct() {
     console.log('productForm', this.productForm);
@@ -44,6 +46,10 @@ export class NewProduct {
     });
 
 
+  }
+
+  cancel(){
+    this._router.navigate(['/products']);
   }
 
   onFileSelected(event: Event) {
